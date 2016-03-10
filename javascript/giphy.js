bc77b1cd905b3f93403208540e4bb27bbe31078e
Zelaway <zelaway@me.com>
2016-03-09 21:48:25 -0500

    new

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,36 +1,32 @@
-//alert("hi");
-
-//Globla Variables:
-var musicians = ['Nina Simone', 'Neyo', 'Madonna', 'Tina', 'Beyonce', 'Cher', 'Michael Jackson', 'Prince', 'Elton John', 'Tony Benet'];
-
-//functions
-//----------------------
- function startMusicians(){
- 
-   for (var i = 0; i < musicians.length; i++){
-
-        
-        /*
-            Write code between the dashes below to 
-                * make a button    
-                    * give the button a class of movie
-                    * set the data-name attribute of the button to movie name
-                    * set the text of the button to the movie name 
-                * append the button to moviesView
-        */
-
-        //------YOUR CODE GOES IN THESE DASHES
-        if(musicians.length==10){
-        $("#originals").append("<button type='button' class='btn btn-default'>" + musicians[i] + "</button>");
-	    }//else if (musicians.length>10){
-
-	    // }
+   var person = ['Nina Simone', 'Neyo', 'Madonna', 'Tina', 'Beyonce', 'Cher', 'Michael Jackson', 'Prince', 'Elton John', 'Tony Benet'];
+    function renderButtons(){ 
+        $('#buttonsView').empty();
+        for (var i = 0; i < person.length; i++){
+            var a = $('<button>')
+            a.addClass('movie');
+            a.attr('data-name', person[i]);
+            a.html(person[i]);
+            $('#buttonsView').append(a);
+        }
     }
-}
-$("#searchButton").click(function() {
-  var newPerson;
-  musicians.push(newPerson);
-  console.log(musicians);
-
-});
-startMusicians();
+    function displayMovieInfo(){
+        var person = $(this).attr('data-name');
+        //var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ person +"&limit=10&rating=g&api_key=dc6zaTOxFJmzC";
+        //Write code between the dashes below to hit the queryURL, take the data and display it in the div with an id of moviesView
+        
+        //------YOUR CODE GOES IN THESE DASHES
+        $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+             $("#personsView").prepend(JSON.stringify(response));
+        }); 
+        //------
+    }
+    renderButtons();
+    $('#addPerson').on('click', function(){
+        var movie = $('#movie-input').val().trim();
+        movies.push(movie);
+        
+        renderButtons();
+        return false;
+    });
+    $(document).on('click', '.movie', displayMovieInfo);
